4e8631a6ad10df5f78e5c83dbddd8345be8634bc
唯二 <vear.vip@qq.com>
2024-05-15 07:11:20 +0000

    更新1.0.5压缩json

--- src/dataSource/txt2Json.js
+++ src/dataSource/txt2Json.js
@@ -1,11 +1,10 @@
 const fs = require("fs");
 const readline = require("readline");
 const path = require("path");
-const { propertyList } = require('../utils/index')
+const { propertyList } = require("../utils/index");
 
 const inputFilePath = path.join(__dirname, "Unihan_Readings.txt");
-const outputFilePath = path.join(__dirname, "Unihan_Readings.json")
-
+const outputFilePath = path.join(__dirname, "Unihan_Readings.json");
 
 async function convertUnihanReadingsToJson(inputPath, outputPath) {
   const fileStream = fs.createReadStream(inputPath, { encoding: "utf8" });
@@ -18,7 +17,7 @@ async function convertUnihanReadingsToJson(inputPath, outputPath) {
 
   for await (const line of rl) {
     if (!line) {
-      break
+      break;
     }
     const trimmedLine = line.trim();
     if (trimmedLine) {
@@ -35,7 +34,7 @@ async function convertUnihanReadingsToJson(inputPath, outputPath) {
               const propertyIndex = propertyList.indexOf(property);
               if (!readings[char]) {
                 readings[char] = [];
-              } 
+              }
               readings[char][propertyIndex] = value;
             }
           }
@@ -43,12 +42,13 @@ async function convertUnihanReadingsToJson(inputPath, outputPath) {
       }
     }
   }
- 
 
   // 修改为根据输出路径参数写入文件
-  await fs.promises.writeFile(outputPath, JSON.stringify(readings, null, 2));
+  await fs.promises.writeFile(outputPath, JSON.stringify(readings));
 
-  console.log(`转换完成，输出文件已保存。转换了${Object.keys(readings).length}个有用汉字。`);
+  console.log(
+    `转换完成，输出文件已保存。转换了${Object.keys(readings).length}个有用汉字。`,
+  );
 }
 
 // 调用函数时可以传入自定义的输出路径，不传则默认保存在同一目录
